c8d228a5881873cbcbcfa81de9c78ad8dcdad4f0
Fury_inc <frilancer029@gmail.com>
2024-12-22 11:04:00 +0500

    Update settings.py for environment variables and deployment

--- OrganiTask/settings.py
+++ OrganiTask/settings.py
@@ -65,8 +65,8 @@ MIDDLEWARE = [
 ]
 
 # Allow requests from any domain for testing (for production, be more specific)
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = ['https://drf-api-n9oj.onrender.com']
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['https://drf-api-n9oj.onrender.com', 'http://127.0.0.1:5500', 'http://localhost:5500']
 
 
 
