bf0581541f1c63a945c3c9cb199b2b7b71d5f047
Fury_inc <frilancer029@gmail.com>
2025-01-13 03:05:08 +0500

    Made several changes

--- OrganiTask/settings.py
+++ OrganiTask/settings.py
@@ -214,7 +214,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
     'ALGORITHM': 'HS256',
